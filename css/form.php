216abedad3bec4d554e581f68b3b9b253f0d2290
jcscrip <sacripantijc@gmail.com>
2024-11-06 12:37:50 -0300

    Add files via upload

---
+++ css/form.php
@@ -0,0 +1,16 @@
+<?php
+
+$email=$_POST["email"];
+$submit=$_POST["submit"];
+$nombre=$_POST["nombre y mensaje"];
+
+$mensaje="Este mensaje fue enviado por:".$nombre."\r\n";
+$mensaje.="Su e-mail es:".$email."\r\n";
+$mensaje.="Mensaje:".$_POST["nombre"]."\r\n";
+$mensaje.="Enviado el:".date('d/n/y, time()');
+
+$para="sacripantijc@gmail.com";
+$asunto="mensaje de sitio web";
+
+mail($para, $asunto, utf8_decode($mensaje),$header);
+header("location:exito.html");
